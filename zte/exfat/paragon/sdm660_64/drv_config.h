18a8c401fa13fc31fc7db230f7da361b097534bc
blurzyy <selster02@gmail.com>
2023-03-14 15:52:40 +0800

    m

--- zte/exfat/paragon/sdm660_64/drv_config.h
+++
@@ -1,3 +0,0 @@
-#define PACKAGE_TAG "lke_9.5.5_b5"
-#define UFSD_TRACE 1
-#define UFSD_BUILTINT_UTF8 1
--- zte/ntfs-3g/moduleconfig.mk
+++
@@ -1,8 +0,0 @@
-# ZTE_MODIFY start for NTFS tools by yangshentao
-ifeq (true,$(ZTE_NTFS_SUPPORT))
-PRODUCT_PACKAGES += \
-    fsck.ntfs \
-    mkfs.ntfs \
-    mount.ntfs
-endif
-# ZTE_MODIFY end for NTFS tools by yangshentao
